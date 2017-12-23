c074ecd82e84725ea4c6d7640ba49a5553609f2b
Aqib Naveed <aqibn1996@gmail.com>
2017-12-23 12:08:03 +0500

    Messages sent & received, but list is not updating at that time.

---
+++ app/src/main/java/mhd3v/filteredsms/QuickResponseService.java
@@ -0,0 +1,8 @@
+package mhd3v.filteredsms;
+
+/**
+ * Created by Saqib Naveed on 21-Dec-17.
+ */
+
+public class QuickResponseService {
+}
---
+++ app/src/main/java/mhd3v/filteredsms/SmsBroadcastReceiver.java
@@ -0,0 +1,8 @@
+package mhd3v.filteredsms;
+
+/**
+ * Created by Saqib Naveed on 20-Dec-17.
+ */
+
+public class SmsBroadcastReceiver {
+}
