1af86449863099b078971975c368de0a337fe881
Aqib Naveed <aqibn1996@gmail.com>
2017-12-23 22:45:45 +0500

    New Message activity added - Can get contact's name from list of contacts

---
+++ app/src/main/java/mhd3v/filteredsms/NewMessage.java
@@ -0,0 +1,13 @@
+package mhd3v.filteredsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+    }
+}
