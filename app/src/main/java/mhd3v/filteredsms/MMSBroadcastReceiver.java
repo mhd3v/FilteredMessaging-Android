a299b53a76e8893a35ae7aa1f4af7371420168bd
Mahad <mamir3012@gmail.com>
2017-12-25 11:34:15 +0500

    Can now be set as the default SMS app

---
+++ app/src/main/java/mhd3v/filteredsms/MMSBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package mhd3v.filteredsms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Mahad on 12/25/2017.
+ */
+
+public class MMSBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String SMS_BUNDLE = "pdus";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        throw new UnsupportedOperationException("Coming soon!");
+    }
+
+}
