f95dbe23747a3756b5523b950df4ac90d7f6ab51
Mahad <mamir3012@gmail.com>
2018-03-28 14:07:07 +0500

    Added activities which ask to change default application and allow permissions

---
+++ app/src/main/java/mhd3v/filteredsms/PermissionsActivity.java
@@ -0,0 +1,120 @@
+package mhd3v.filteredsms;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    private static final int READ_SMS_PERMISSIONS_REQUEST = 1;
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 2;
+    Intent MainIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+
+    }
+
+    public void nextClicked(View view) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED)
+            getPermissionToReadSMS();
+
+        else if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED)
+            getPermissionToReadContacts();
+    }
+
+    public void getPermissionToReadSMS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_SMS));
+
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.READ_SMS},
+                        READ_SMS_PERMISSIONS_REQUEST);
+            }
+        }
+
+    }
+
+    public void getPermissionToReadContacts() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                        READ_CONTACTS_PERMISSIONS_REQUEST);
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+
+
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_SMS_PERMISSIONS_REQUEST) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED)
+                    getPermissionToReadContacts();
+
+                else{
+                    finish();
+                    MainIntent = new Intent(this, MainActivity.class);
+                    MainIntent.putExtra("cameFromFirst", true);
+                    startActivity(MainIntent);
+                }
+
+            }
+
+        }
+
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST ) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)
+                        != PackageManager.PERMISSION_GRANTED)
+                    getPermissionToReadSMS();
+
+                else{
+                    finish();
+                    MainIntent = new Intent(this, MainActivity.class);
+                    MainIntent.putExtra("cameFromFirst", true);
+                    startActivity(MainIntent);
+                }
+
+            }
+
+        }
+
+    }
+
+    public void exitClicked(View view) {
+        finish();
+    }
+}
